ec7d3aac0bfe551ee25b83e664a2ce6fc04aaf19
skshabeera <shabeera20@navgurukul.org>
2022-04-03 00:58:44 +0530

    initial commit

---
+++ routes/api/user.js
@@ -0,0 +1,66 @@
+const express=require("express")
+const router = express.Router()
+const { check,validationResult } = require('express-validator')
+const bcrypt = require('bcryptjs')
+const User=require('../../models/User')
+
+
+router.post('/',[
+    check('name','Name is required').not().isEmpty(),
+    check('email','please include a valid email').isEmail(),
+    check('password','please enter a passsword with 6 or more characters').isLength({min:6,max:12})
+
+],
+async(req,res)=> {
+
+    const errors=validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array() })
+    } 
+    
+    const {name,email,password}=req.body
+    try{
+        let user = await User.findOne({email})
+        if(user){
+            res.status(400).json({errors:[{msg:'User already exists'}]});
+
+    }
+    user = new User({
+        name,
+        email,
+        password
+    })
+    const salt = await bcrypt.genSalt(10)
+    user.password = await bcrypt.hash(password,salt)
+    await user.save()
+
+    res.send("User registered")
+
+    }catch(err){
+        console.error(err.message)
+        res.status(500).send("server error")
+
+
+    }
+})
+    router.get('/:id',async(req,res) =>{
+        try{
+            const user = await User.findById( req.params.id )
+            if (!user){
+                return res.status(404).json( {  msg :'user not found'})
+            }
+            res.json(user)
+    
+        }catch(err){
+            console.error(err.message) 
+            if  (err.kind === 'ObjectId'){
+                return res.status(404).json( {  msg :'user not found'})
+        }
+        res.status(500).send("server Error")
+        }
+
+})
+
+
+
+module.exports = router
